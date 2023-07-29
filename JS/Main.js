80205bb7d42ea6a6eed8c50f136cd1647143ea48
mahmoud123-stack <126006347+mahmoud123-stack@users.noreply.github.com>
2023-07-29 21:44:17 +0300

    Add files via upload

---
+++ JS/Main.js
@@ -0,0 +1,50 @@
+let Main_title = "Elzero",
+  Main_desc = "Elzero web school",
+  Main_date = "25/10";
+
+let Page = `
+<div class="Parent">
+  <h3>${Main_title}</h3>
+  <p>${Main_desc}</p>
+  <span>${Main_date}</span>
+</div>
+
+<div class="Parent">
+  <h3>${Main_title}</h3>
+  <p>${Main_desc}</p>
+  <span>${Main_date}</span>
+</div>
+
+<div class="Parent">
+  <h3>${Main_title}</h3>
+  <p>${Main_desc}</p>
+  <span>${Main_date}</span>
+</div>
+
+<div class="Parent">
+  <h3>${Main_title}</h3>
+  <p>${Main_desc}</p>
+  <span>${Main_date}</span>
+</div>
+`;
+
+document.write(Page);
+// ---------------------------------------
+
+console.log("`I'm In\n \\\\ \n Love \\\\ \"\"\" '''\n ++ with ++ \n \\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``")
+
+
+// let a = 21;
+// let b = 20;
+
+// _21_2021_2021_2021_20_
+
+// console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`); 
+
+let a = 10 ;
+let b = "10" ; 
+let c = 80 ;
+
+console.log(++a + +b++ + +c++ - +a++ );
+
+
